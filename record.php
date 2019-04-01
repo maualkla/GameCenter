0b284740c52b5af7b8aa82cb8bc8d52ec2768155
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-01 14:30:57 -0600

    if

--- record.php
+++ record.php
@@ -7,8 +7,18 @@ $user = "";
 
 // Getting params
 $game = $_POST['game'];
+$record = $_POST['record'];
+$user = $_POST['user'];
 
 // Validating params
+if($game == "1")
+{
+	//Sudoku Party
+}
+elseif($game == "0")
+{
+	//5 in Line
+}
 
 // Creating record
 
