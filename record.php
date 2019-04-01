f7014f3515259328cabc47f37c18d51d578cfeca
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-03-31 22:07:13 -0600

    creating_php

---
+++ record.php
@@ -0,0 +1,19 @@
+<?php
+
+// Defining variables
+$game = "";
+$record = "";
+$user = "";
+
+// Getting params
+$game = $_POST['game'];
+
+// Validating params
+
+// Creating record
+
+// Notifing record
+
+// Redirect to page
+
+?>
