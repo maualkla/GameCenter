93856a55d95c183578728ad7f01901680e2a021b
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-04 22:04:32 -0600

    enfuncion

--- record.php
+++ record.php
@@ -14,54 +14,75 @@ $record = $_REQUEST['record'];
 $user = $_REQUEST['user'];
 
 echo " : ".$operation." : ".$game." : ".$record." : ".$user;
-// Validating params
-$record = intval($record);
-echo " TYPE : ".gettype($record);
-if(gettype($record) == "integer")
+
+
+// Conexion
+function conexion()
 {
-	$record = intval($record);
-	if(strlen($user) < 3)
+	echo " Everything is fine creando conexion...";
+	$realHost = "sql208.epizy.com:3306";
+	$realUser = "epiz_23620501";
+	$realPass = "Ajedrez101";
+	$localHost = "127.0.0.1:3306";
+	$localUser = "root";
+	$localPass = "";
+	$dbc = mysqli_connect($localHost,$localUser,$localPass,"records");
+	if (!$dbc) 
 	{
-		$final = 1;
-		echo ' ERROR: No se puto validar el nombre de usuario.';
+		die("Error de conexion: " . mysqli_connect_error());
 	}
 	else
 	{
-		echo " Everything is fine creando conexion...";
-		$realHost = "sql208.epizy.com:3306";
-		$realUser = "epiz_23620501";
-		$realPass = "Ajedrez101";
-		$localHost = "127.0.0.1:3306";
-		$localUser = "root";
-		$localPass = "";
-
-		$dbc = mysqli_connect($localHost,$localUser,$localPass,"records");
-    	if (!$dbc) 
-    	{
-        	die("Error de conexion: " . mysqli_connect_error());
-    	}
-    	else
-    	{
-    		//echo " Conexion exitosa!!!";
-    	}
+		echo " Conexion exitosa!!!";
 	}
+	return $dbc;
+}
+
+
+if($operation == "1")
+{
+	$dbc = conexion();
+	// Validating params
+	$record = intval($record);
+	echo " TYPE : ".gettype($record);
+	if(gettype($record) == "integer")
+	{
+		$record = intval($record);
+		if(strlen($user) < 3)
+		{
+			$final = 1;
+			echo ' ERROR: No se puto validar el nombre de usuario.';
+		}
+		else
+		{
+			//success
+		}
+	}
+	else
+	{
+		$final = 1;
+		echo ' ERROR: No se pudo validar el puntaje.';
+	}
+	if($final == 0)
+	{
+		//create query
+		$sql = 'INSERT INTO records (game, record, user) VALUES ("'.$game.'", "'.$record.'", "'.$user.'")';
+		//execute query
+		//show response
+	}
+}
+elseif($operation == "2")
+{
+	$dbc = conexion();
 }
 else
 {
-	$final = 1;
-	echo ' ERROR: No se pudo validar el puntaje.';
+	$dbc = null;
 }
+	// Creating record
+	
 
-// Creating record
-if($final == 0)
-{
-	//conncect
-	$sql = 'INSERT INTO records (game, record, user) VALUES ("'.$game.'", "'.$record.'", "'.$user.'")';
-	//execute query
-	//show response
-}
-
-// Display records
+	// Display records
 
 
 // Notifing record
